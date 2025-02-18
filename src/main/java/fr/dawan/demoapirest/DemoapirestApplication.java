8ab96f3e8fbdb49ea45e9613333ce07036b16262
Mourad <mmahrane@dawan.fr>
2025-02-18 13:12:37 +0100

    Jour2-Pause-Midi

--- src/main/java/fr/dawan/demoapirest/DemoapirestApplication.java
+++ src/main/java/fr/dawan/demoapirest/DemoapirestApplication.java
@@ -1,14 +1,30 @@
 package fr.dawan.demoapirest;
 
+import fr.dawan.demoapirest.tools.Contact;
+import fr.dawan.demoapirest.tools.ContactDto;
+import fr.dawan.demoapirest.tools.ModelMapperDemo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoapirestApplication {
+public class DemoapirestApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoapirestApplication.class, args);
 
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(">>>>>> test ModelMapper<<<<<<<<<<<<");
+
+		Contact c = new Contact();
+		c.setFirstName("DUPONT");
+		c.setLastName("Jean");
+		ContactDto dto = ModelMapperDemo.dtoFormEntity(c);
+
+		System.out.println(dto);
+
+	}
 }
